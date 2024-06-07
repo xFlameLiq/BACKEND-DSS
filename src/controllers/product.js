f58a2b23a5ef0f1fa35a5b420c770b33a1b6c725
xFlameLiq <benjamincoredev@outlook.com>
2024-06-06 20:05:38 -0600

    crud backend terminado

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -1,38 +1,57 @@
 const db = require('../config/db.config');
 
 exports.getProducts = (req, res) => {
-
+    const sql = 'SELECT * FROM products';
+    db.query(sql, (err, results) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ message: "Error al obtener los productos de la base de datos" });
+        }
+        return res.status(200).json(results);
+    });
 };
 
 exports.createProduct = (req, res) => {
     const { name, model, price } = req.body;
-    
-    // Verificar que todos los campos necesarios estén presentes
     if (!name || !model || !price) {
         return res.status(400).json({ message: "Todos los campos son requeridos" });
     }
-
-    // Crear la consulta SQL de inserción
     const sql = 'INSERT INTO products (name, model, price) VALUES (?, ?, ?)';
-    
-    // Ejecutar la consulta SQL con los valores proporcionados
     db.query(sql, [name, model, price], (err, result) => {
         if (err) {
-            // Si ocurre un error al ejecutar la consulta, enviar una respuesta de error
             console.error(err);
             return res.status(500).json({ message: "Error al insertar el producto en la base de datos" });
         }
-
-        // Si la inserción fue exitosa, enviar una respuesta de éxito
         return res.status(201).json({ message: "Producto insertado exitosamente" });
     });
 };
 
-
 exports.deleteProduct = (req, res) => {
-
+    const { id } = req.body;
+    if (!id) {
+        return res.status(400).json({ message: "El ID del producto es requerido" });
+    }
+    const sql = 'DELETE FROM products WHERE id = ?';
+    db.query(sql, [id], (err, result) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ message: "Error al eliminar el producto de la base de datos" });
+        }
+        return res.status(200).json({ message: "Producto eliminado exitosamente" });
+    });
 };
 
 exports.updateProduct = (req, res) => {
-
+    const { id, name, model, price } = req.body;
+    if (!id || !name || !model || !price) {
+        return res.status(400).json({ message: "Todos los campos son requeridos" });
+    }
+    const sql = 'UPDATE products SET name = ?, model = ?, price = ? WHERE id = ?';
+    db.query(sql, [name, model, price, id], (err, result) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ message: "Error al actualizar el producto en la base de datos" });
+        }
+        return res.status(200).json({ message: "Producto actualizado exitosamente" });
+    });
 };
